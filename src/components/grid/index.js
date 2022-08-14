654ef09ec341a3af689097f00a5197ac7d075c52
TantoFaz <vinybergamo@gmail.com>
2022-08-13 23:31:17 -0300

    first commit

---
+++ src/components/grid/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import GridItem from "../gridItem";
+import * as C from "./styles";
+
+const Grid = ({ items, setItens }) => {
+  const onDelete = (ID) => {
+    const newArray = items.filter((transaction) => transaction.id !== ID);
+    setItens(newArray);
+    localStorage.setItem("Transactions", JSON.stringify(newArray));
+  };
+
+  return (
+    <C.Table>
+      <C.Thead>
+        <C.Tr>
+          <C.Th width={40}>Descrição</C.Th>
+          <C.Th width={40}>Valor</C.Th>
+          <C.Th width={10} alignCenter>
+            Tipo
+          </C.Th>
+        </C.Tr>
+      </C.Thead>
+      <C.Tbody>
+        {items?.map((item, index) => (
+          <GridItem key={index} item={item} onDelete={onDelete} />
+        ))}
+      </C.Tbody>
+    </C.Table>
+  );
+};
+export default Grid;
